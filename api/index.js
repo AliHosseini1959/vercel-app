618ba0c08d3cab0fb0e9413ad588d12705186a98
AliHosseini1959 <ali.hosseini@primorconsulting.com>
2025-04-07 12:11:47 +0300

    14

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("<H1 style=color:blue;>Express on Vercel is working! 13</H1>"));
+app.get("/", (req, res) => res.send("<H1 style=color:blue;>Express on Vercel is working! 14</H1>"));
 //app.listen(4000, () => console.log("Server ready on port 4000."));
+app.get("/api/data", (req, res) => {
+  res.json(getData());
+});
 module.exports = app;
